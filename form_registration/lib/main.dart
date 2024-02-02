760205a9054ee546b8f831c0aa19ef17d43d3a48
Dimon <urvandimon@gmail.com>
2024-02-02 12:11:58 +0300

    //

--- form_registration/lib/main.dart
+++ form_registration/lib/main.dart
@@ -9,7 +9,7 @@ import 'package:form_registration/servises/auth_servises.dart';
 
 import 'core/app_export.dart';
 
-//
+///
 
 var globalMessengerKey = GlobalKey<ScaffoldMessengerState>();
 Future<void> main() async {
